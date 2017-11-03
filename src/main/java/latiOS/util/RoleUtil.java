28ec7da264886298ae86321d6c53672a0265cb5c
myles <hornfre000@MHSR107-HT1452.sbm1.springbranchisd.com>
2017-11-03 15:00:09 -0500

    uhhhh role stuff

--- src/main/java/latiOS/util/RoleUtil.java
+++ src/main/java/latiOS/util/RoleUtil.java
@@ -7,6 +7,9 @@ import java.util.Set;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.requests.Request;
+import net.dv8tion.jda.core.requests.Response;
+import net.dv8tion.jda.core.requests.RestAction;
 import net.dv8tion.jda.core.utils.SimpleLog;
 
 public class RoleUtil {
@@ -29,16 +32,21 @@ public class RoleUtil {
 		}
 	}
 	
-	public void checkUserRoles() {
+	public synchronized void checkUserRoles() {
 		List<Member> mems = guild.getMembers();
-		log.trace("P1=userRoles");
 		mems.forEach(i->{
-			log.trace("P2=loop::"+i.getRoles().stream().anyMatch(k->k.getName().equals(i.getUser().getName())));
-			/*if (!i.getRoles().stream().anyMatch(k->k.getName().equals(i.getUser().getName()))) {
-				Role r = guild.getController().createRole().complete();
-				r.getManager().setName(i.getUser().getName()).complete();
-				guild.getController().addSingleRoleToMember(i, r);
-			}*/
+			if (!i.getUser().isBot()) {
+				Role r;
+				if (!guild.getRoles().stream().anyMatch(k->k.getName().equals(i.getUser().getName()))) {
+					r = guild.getController().createRole().complete();
+					r.getManager().setName(i.getUser().getName()).complete();
+				}else {
+					r = guild.getRolesByName(i.getUser().getName(), false).get(0);
+				}
+				if (!i.getRoles().stream().anyMatch(k->k.getName().equals(i.getUser().getName()))) {
+					guild.getController().addSingleRoleToMember(i, r).complete();	
+				}
+			}
 		});
 	}
 }
